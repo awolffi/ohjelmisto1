92f873aa026fd2645a14f3fd4cc8be0bdd4db367
Alexander Wolff <awfinland@gmail.com>
2024-08-21 14:54:41 +0300

    I added ex. 5 and 6 to the repository

---
+++ module_3/module3_1.py
@@ -0,0 +1,7 @@
+pituus = int(input("syota kuhan pituus (cm): "))
+
+if pituus < 37:
+    print("Kuha on alamittainen. Heita takaisin veteen! ")
+    print(f"{37-pituus} cm liian pieni oli")
+else:
+    print("ei tarvi laskea veteen, saat syoda!")
