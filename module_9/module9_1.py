ab282238af2ab7f534362848cd2eadb0f1c681b7
Alexander Wolff <awfinland@gmail.com>
2024-10-23 16:16:49 +0300

    first exercise fix

--- module_9/module9_1.py
+++ module_9/module9_1.py
@@ -1,11 +1,11 @@
 class Car:
-    def __init__(self, registration_number, max_speed, cur_speed=0, distance=0):
+    def __init__(self, registration_number, max_speed,):
         self.registration_number = registration_number
         self.max_speed = max_speed
-        self.cur_speed = cur_speed
-        self.distance = distance
+        self.cur_speed = 0
+        self.distance = 0
 
 car1 = Car("ABC-123", 142)
 
-print(f"Registration number: {car1.registration_number}, max speed:{car1.max_speed}, current speed:{car1.cur_speed}, distance: {car1.distance}")
+print(f"Registration number: {car1.registration_number}, max speed: {car1.max_speed}, current speed: {car1.cur_speed}km/h, distance: {car1.distance}")
     
