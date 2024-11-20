d3bb5b95206f9c525f2d3f393d1e742ef2889c74
Alexander Wolff <awfinland@gmail.com>
2024-11-20 22:41:17 +0200

    module13_1.py push

---
+++ module_13/module13_1.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/alkuluku/<int:num>', methods=['GET'])
+def check_if_prime(num):
+    
+    if num > 1:
+        for x in range(2, (num//2)+1):
+            if num % x == 0:
+                is_prime = False
+                break
+        else:
+            is_prime = True
+    else:
+        is_prime = False
+
+    response = {
+        "number" : num,
+        "isPrime" : is_prime
+    }
+
+    return response
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
