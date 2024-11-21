603f6059efe7faf67b4c53ca144a71ea9853c3ee
Alexander Wolff <awfinland@gmail.com>
2024-11-21 06:54:58 +0200

    small tweaks module12_1 and 12_2

--- module_12/module12_1.py
+++ module_12/module12_1.py
@@ -1,6 +1,6 @@
 import requests
 
-request = "https://api.chucknorris.io/jokes/random"
-response = requests.get(request).json()
+url = "https://api.chucknorris.io/jokes/random"
+response = requests.get(url).json()
 
 print(response["value"])
--- module_12/module12_2.py
+++ module_12/module12_2.py
@@ -6,7 +6,7 @@ city = input("enter city name: ")
 try:
     request = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}"
     response = requests.get(request).json()
-    print(f"in city {city} the weather status is: {response["weather"][0]["description"]} and the temperature is: {(response["main"]["temp"]) - 273.15:.2f} degrees celcius")
+    print(f"in city {city} the weather status is: {response['weather'][0]['description']} and the temperature is: {(response['main']['temp']) - 273.15:.2f} degrees celcius")
 
 except:
     print("didnt work")
